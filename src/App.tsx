685af4139d2ca4dba7d59ce50ef70fc5977727d5
Pedro Fernandez <92454843+PandaDesigner@users.noreply.github.com>
2024-05-12 07:05:35 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Text = styled.p`
+  ${tw`text-3xl font-bold text-indigo-500`}
+`;
+
+const ContainerBg = styled.div`
+${tw`p-3 bg-slate-400 shadow-lg rounded-lg text-white`}
+`
+
+function App() {
+  return (
+    <>
+      <Text as="h1">hola mundo</Text>
+
+      <div tw='space-y-5 p-5'>
+        <ContainerBg>
+          <h3 tw="text-xs border-b">font-sans</h3>
+          <p tw="font-sans">
+            The quick brown fox jumps over the lazy dog.
+          </p>
+        </ContainerBg>
+      </div>
+    </>
+  );
+}
+
+export default App;
